d019278191c423612bd70c96c3e46240eb9324b0
tavo1987 <tavo198718@gmail.com>
2018-03-21 15:47:44 -0500

    initial commit

---
+++ application/Bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+
+    protected function _initAutoload()
+    {
+        $modelLoader = new Zend_Application_Module_Autoloader(array(
+            'basePath' => APPLICATION_PATH,
+            'namespace' => '',
+        ));
+
+        return $modelLoader;
+    }
+
+    protected function _initDoctype()
+    {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $view->doctype('HTML5');
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Zend Framework Version: 1.11.11
+
+Esto es un un repositorio de pruebas que he creado por un requerimiento especifíco de acutalización de una app que usa esta versión.
+Siguiendo el [tutorial](https://www.youtube.com/watch?v=UiG2bCmZt-I&list=PL90503BB7541343E9&index=1) que se encuentra
+en youtube. 
+
+    Esta es una versión obsoleta del framework.  
